bf3902b069d0516b275a1b2415769a97147f198c
ChuckBTaylor <WLScripps@gmail.com>
2017-10-11 09:19:59 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function Driver(name, age, experience) {
+  this.age = age;
+  this.name = name;
+  this.experience = experience;
+}
+
+function Scooter(year, color, model) {
+  this.model = model;
+  this.color = color;
+  this.year = year;
+}
+
+function PickupLocation(address, city) {
+  this.address = address;
+  this.city = city;
+}
